55b25c6b059d28bca6058b8f0f6dad1a3100d53c
John Munyi <wachirawamunyi@gmail.com>
2017-10-28 14:50:43 +0200

    moved notification code to do in background to simulate snooze

--- app/src/main/java/com/example/johnmunyi/getupandwalk/MainActivity.java
+++ app/src/main/java/com/example/johnmunyi/getupandwalk/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.johnmunyi.getupandwalk;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -28,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 String timeSet = timeToTrack.getText().toString();
-                Tracking runner = new Tracking();
+                Tracking runner = new Tracking(getApplicationContext());
                 runner.execute(timeSet);
             }
         });
--- app/src/main/java/com/example/johnmunyi/getupandwalk/Tracking.java
+++ app/src/main/java/com/example/johnmunyi/getupandwalk/Tracking.java
@@ -1,6 +1,18 @@
 package com.example.johnmunyi.getupandwalk;
 
+import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.AsyncTask;
+import android.os.Vibrator;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.NotificationCompat;
+
+import static android.content.Context.NOTIFICATION_SERVICE;
+import static android.content.Context.VIBRATOR_SERVICE;
 
 /**
  * Created by johnmunyi on 10/26/17.
@@ -9,14 +21,26 @@ import android.os.AsyncTask;
 public class Tracking extends AsyncTask<String, String, String> {
 
     private String resp;
+    private Context context;
+
+    Tracking(Context context)
+    {
+        this.context = context;
+    }
 
     @Override
     protected String doInBackground(String... params){
         try
         {
             int trackedTime = Integer.parseInt(params[0])*1000;
-            Thread.sleep(trackedTime);
-            resp = "Slept for " + params[0] + "seconds";
+
+            while (trackedTime > 0)
+            {
+                Thread.sleep(trackedTime);
+                resp = "Slept for " + params[0] + "seconds";
+                runNotification();
+            }
+
         }
         catch(Exception e)
         {
@@ -28,11 +52,7 @@ public class Tracking extends AsyncTask<String, String, String> {
 
     @Override
     protected void onPostExecute(String s) {
-        // send notification to the screen
         super.onPostExecute(s);
-        NotificationFlash myNotify = new NotificationFlash();
-        myNotify.showNotification();
-        System.out.println(resp);
     }
 
     @Override
@@ -43,5 +63,27 @@ public class Tracking extends AsyncTask<String, String, String> {
     @Override
     protected void onProgressUpdate(String... values) {
         super.onProgressUpdate(values);
+    }
+
+    protected void runNotification()
+    {
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+        builder.setSmallIcon(android.R.drawable.ic_dialog_alert);
+        builder.setContentTitle("Get up and Walk");
+        builder.setContentText("Been seated for too long time has expired");
+        builder.setDefaults(Notification.DEFAULT_LIGHTS | Notification.DEFAULT_SOUND);
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0,intent, 0);
+        builder.setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(1,builder.build());
+
+        Vibrator v = (Vibrator) context.getSystemService(VIBRATOR_SERVICE);
+        v.vibrate(200);
+
+        System.out.println(resp);
     }
 }
